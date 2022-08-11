dd783dc85e462f3215a407303e78b71c5122a8cc
chengqiangxj@163.com <cuit_2019!>
2022-08-11 12:14:21 +0800

    Site updated: 2022-08-11 12:13:06

---
+++ js/weather.js
@@ -0,0 +1,23 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "5",
+    "tmpColor": "9900FF",
+    "tmpSize": "16",
+    "cityColor": "00FFFF",
+    "citySize": "16",
+    "aqiColor": "00FF00",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "zh",
+    "fixed": "true",
+    "vertical": "top",
+    "horizontal": "left",
+    "left": "160",
+    "top": "10",
+    "key": "8628434f8f194eccb94ff3fecb1486de"
+  }
+}
